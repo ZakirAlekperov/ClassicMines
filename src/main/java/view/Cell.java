706857c3bbe1be682fb3497cb7db0965a073186d
zakiralekperov <zakiralekperov1998@gmail.com>
2023-06-18 10:45:50 +0300

    создан класс клетки, добавлены основные пустые методы

---
+++ src/main/java/view/Cell.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.awt.*;
+
+public class Cell {
+    public void paint(Graphics g, int x, int y){
+
+    }
+
+    public void inverseFlag() {
+    }
+
+    public boolean isNotOpen() {
+        return true;
+    }
+
+    public void open() {
+    }
+
+    public int getCountBomb() {
+        return 0;
+    }
+
+    public boolean isMined() {
+        return true;
+    }
+
+    public void mine() {
+    }
+
+    public void setCountBomb(int count) {
+    }
+}
