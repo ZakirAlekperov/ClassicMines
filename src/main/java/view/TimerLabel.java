5824837831dc401349ae586486beec38421fff57
zakiralekperov <zakiralekperov1998@gmail.com>
2023-06-18 10:27:32 +0300

    добавлен класс для таймера игры

---
+++ src/main/java/view/TimerLabel.java
@@ -0,0 +1,30 @@
+package view;
+
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerLabel extends JLabel { // label with stopwatch
+        Timer timer = new Timer();
+        public TimerLabel() {
+            timer.scheduleAtFixedRate(timerTask, 0, 1000);
+        }
+
+        TimerTask timerTask = new TimerTask() {
+            volatile int time;
+            Runnable refresher = new Runnable() {
+                    public void run() {
+                        TimerLabel.this.setText(String.format("%02d:%02d", time / 60, time % 60));
+                    }
+                };
+            public void run() {
+                time++;
+                SwingUtilities.invokeLater(refresher);
+            }
+        };
+
+        public void stopTimer() {
+            timer.cancel();
+        }
+
+}
