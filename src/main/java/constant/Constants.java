9875bf195320e22e682ee3d5891c704a57a05322
zakiralekperov <zakiralekperov1998@gmail.com>
2023-06-18 10:11:35 +0300

    добавлен класс с константами

---
+++ src/main/java/constant/Constants.java
@@ -0,0 +1,15 @@
+package constant;
+
+public class Constants {
+    public static final String TITLE_OF_PROGRAM = "Mines";
+    public static final String SIGN_OF_FLAG = "f";
+    public static final int BLOCK_SIZE = 30; // size of one block
+    public static final int FIELD_SIZE = 9; // in blocks
+    public static final int FIELD_DX = 6; // determined experimentally
+    public static final int FIELD_DY = 28 + 17;
+    public static final int START_LOCATION = 200;
+    public static final int MOUSE_BUTTON_LEFT = 1; // for mouse listener
+    public static final int MOUSE_BUTTON_RIGHT = 3;
+    public static final int NUMBER_OF_MINES = 10;
+    public static final int[] COLOR_OF_NUMBERS = {0x0000FF, 0x008000, 0xFF0000, 0x800000, 0x0};
+}
